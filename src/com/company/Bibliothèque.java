fa8a2a95be2d2df22ca054f8bf9e60e329b4416e
Koloina <koloina.tiana.rasolofoson@esti.mg>
2021-10-26 19:39:01 +0200

    version 1

---
+++ src/com/company/Bibliothèque.java
@@ -0,0 +1,13 @@
+package com.company;
+
+import java.util.ArrayList;
+
+public class Bibliothèque {
+    ArrayList<PC> machines;
+    ArrayList<Salle> salles;
+
+    public Bibliothèque() {
+        machines = new ArrayList<PC>();
+        salles = new ArrayList<Salle>();
+    }
+}
--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,8 +1,29 @@
 package com.company;
 
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        System.out.println("Bienvenu dans la blibliothèque 2.0");
+
+        System.out.println("Veuillez choisir une action : 1 - Connexion compte client 2 - Inscription nouveau client");
+
+        Scanner scan = new Scanner(System.in);
+
+        int choix = scan.nextInt();
+
+        switch (choix) {
+
+            case 1:
+
+                break;
+
+            case 2:
+
+                break;
+
+
+        }
     }
 }
