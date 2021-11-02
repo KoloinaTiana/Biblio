25a20e6ccf923702a5df2e17e1d8ed46968bd22e
mhamadhajj <93221918+mhamadhajj@users.noreply.github.com>
2021-11-02 13:38:42 +0100

    Update Emprunt.java
    creation

--- src/com/company/Emprunt.java
+++ src/com/company/Emprunt.java
@@ -1,4 +1,23 @@
 package com.company;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Emprunt {
+
+	private Date date;
+	private Date datelimite;
+	private List<Document> dpcuments = new ArrayList<Document>();
+	public Emprunt(Date date, Date datelimite) {
+		this.date = date;
+		this.datelimite = datelimite;
+	}
+	@Override
+	public String toString() {
+		return "Emprunt [date=" + date + ", datelimite=" + datelimite + "]";
+	}
+	
+	
+	
+	
 }
