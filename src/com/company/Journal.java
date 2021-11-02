309bc8242af5c4e8ca43231ba62d4c9ea9ad6e83
mhamadhajj <93221918+mhamadhajj@users.noreply.github.com>
2021-11-02 13:40:42 +0100

    Update Journal.java
    Version 1

--- src/com/company/Journal.java
+++ src/com/company/Journal.java
@@ -1,4 +1,17 @@
 package com.company;
 
-public class Journal {
+public class Journal extends Document {
+	private String nomEditorialiste;
+
+	public Journal(String titre, Date date, String langue, boolean estdisponible, String nomEditorialiste) {
+		super(titre, date, langue, estdisponible);
+		this.nomEditorialiste = nomEditorialiste;
+	}
+
+	@Override
+	public String toString() {
+		return "Journal [nomEditorialiste=" + nomEditorialiste +super.toString()+ "]";
+	}
+	
+	
 }
