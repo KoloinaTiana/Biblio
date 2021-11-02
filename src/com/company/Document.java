be9c79d3392f71c0748bd32a10bc210780ea51a2
mhamadhajj <93221918+mhamadhajj@users.noreply.github.com>
2021-11-02 13:42:37 +0100

    Version 1

--- src/com/company/Document.java
+++ src/com/company/Document.java
@@ -1,4 +1,22 @@
 package com.company;
 
 public class Document {
+	protected String titre;
+	protected Date date;
+	protected String langue;
+	protected boolean estdisponible;
+	public Document(String titre, Date date, String langue, boolean estdisponible) {
+		super();
+		this.titre = titre;
+		this.date = date;
+		this.langue = langue;
+		this.estdisponible = estdisponible;
+	}
+	@Override
+	public String toString() {
+		return "Document [titre=" + titre + ", date=" + date + ", langue=" + langue + ", estdisponible=" + estdisponible
+				+ "]";
+	}
+	
+	
 }
