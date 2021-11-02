761b3a6a9bc5a7d876c4269618974380c5f6fdaa
mhamadhajj <93221918+mhamadhajj@users.noreply.github.com>
2021-11-02 13:42:01 +0100

    Version 1

--- src/com/company/Livre.java
+++ src/com/company/Livre.java
@@ -1,4 +1,26 @@
 package com.company;
 
-public class Livre {
+public class Livre extends Document {
+	private String auteur;
+	protected String sujet;
+	protected String description;
+	protected int edition;
+	protected int nbpage;
+	public Livre(String titre, Date date, String langue, boolean estdisponible, String auteur, String sujet,
+			String description, int edition, int nbpage) {
+		super(titre, date, langue, estdisponible);
+		this.auteur = auteur;
+		this.sujet = sujet;
+		this.description = description;
+		this.edition = edition;
+		this.nbpage = nbpage;
+	}
+	@Override
+	public String toString() {
+		return "Livre [auteur=" + auteur + ", sujet=" + sujet + ", description=" + description + ", edition=" + edition
+				+ ", nbpage=" + nbpage +super.toString()+ "]";
+	}
+	
+	
+	
 }
