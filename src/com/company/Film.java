c0a9745237278d3eb75cf0cd7e6ad2183a3c2d16
mhamadhajj <93221918+mhamadhajj@users.noreply.github.com>
2021-11-02 13:40:02 +0100

    Update Film.java

--- src/com/company/Film.java
+++ src/com/company/Film.java
@@ -1,4 +1,18 @@
 package com.company;
 
-public class Film {
+public class Film extends Document {
+	private String realisateur;
+	private String producteur;
+	public Film(String titre, Date date, String langue, boolean estdisponible, String realisateur, String producteur) {
+		super(titre, date, langue, estdisponible);
+		this.realisateur = realisateur;
+		this.producteur = producteur;
+	}
+	@Override
+	public String toString() {
+		return "Film [realisateur=" + realisateur + ", producteur=" + producteur +super.toString()+ "]";
+	}
+	
+	
 }
+
