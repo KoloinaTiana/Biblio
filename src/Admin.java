0d6ae5a4d672e12d88e934450c1622799db35e70
benji4411 <92875394+benji4411@users.noreply.github.com>
2021-12-13 23:29:02 +0100

    Update Admin.java

--- src/Admin.java
+++ src/Admin.java
@@ -1,5 +1,12 @@
 public class Admin extends Compte{
 
+	/**
+	 * Constructeur de la classe Admin
+	 * @param _identifiant identifiant de l'admin
+	 * @param _motDePasse mot de passe de l'admin
+	 * @param _prenom prenom de l'admin
+	 * @param _nom nom de l'admin
+	 */
 	public Admin(String _identifiant, String _motDePasse, String _prenom, String _nom) {
 		super(_identifiant, _motDePasse,_prenom,_nom);
 		this.prenom = _prenom;
