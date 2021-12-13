d54bd67f5d7f36674348f2e5737fdbe9e75bb324
mhamadhajj <93221918+mhamadhajj@users.noreply.github.com>
2021-12-13 21:34:56 +0100

    javadoc

--- src/Compte.java
+++ src/Compte.java
@@ -6,17 +6,25 @@ public abstract class Compte {
 	protected String prenom;
 	protected String nom;
 
+	
 	public Compte(String _identifiant, String _motDePasse, String _prenom, String _nom) {
 		this.identifiant = _identifiant;
 		this.motDePasse = _motDePasse;
 		this.prenom = _prenom;
 		this.nom = _nom;
 	}
-
+	
+	/** affiche l'identifiant 
+     * @return identifiant
+     */
+	
 	public String getIdentifiant() {
 		return identifiant;
 	}
 
+	/** affiche le mot de passe 
+     * @return le mot de passe
+     */
 	public String getMotDePasse() {
 		return motDePasse;
 	}
