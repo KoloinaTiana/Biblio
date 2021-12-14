a704f5119928ecdc03e56a844b014e437b8b39a4
benji4411 <92875394+benji4411@users.noreply.github.com>
2021-12-14 08:41:39 +0100

    Update Pc.java

--- src/Pc.java
+++ src/Pc.java
@@ -4,11 +4,19 @@ public class Pc {
 	private int id;
 	private boolean estReserve;
 
+	/**
+	 * Constructeur de la classe Pc
+	 * @param _id
+	 */
 	public Pc(int _id) {
 		this.estReserve = false;
 		this.id = _id;
 	}
 
+	/**
+	 * Permet de modifier la valeur de la boolean estReserve
+	 * @param estReserve
+	 */
 	public void setEstReserve(boolean estReserve) {
 		this.estReserve = estReserve;
 	}
